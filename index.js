3553f1cf11fb61e99fd2ace290a5276c061dcd1c
Your Name <you@example.com>
2020-04-26 05:55:14 +1000

    Completed steps 5 and 6

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-//  app.use(express.json)
+ app.use(express.json())
 // app.get('/', (request, response) => {
 //     console.log("Hey")
 //     response.send("<h1>Hello</h1>")
@@ -10,6 +10,9 @@ let persons = [ { name: 'Arto Hellas', number: '040-123456', id:1 },
 { name: 'Ada Lovelace', number: '39-44-5323523', id:2 },
 { name: 'Dan Abramov', number: '12-43-234345', id:3  },
 { name: 'Mary Poppendieck', number: '39-23-6423122' , id:4}];
+
+const generateRandomId = () => Math.floor(Math.random() * 1000000);
+
 
 app.get('/api/persons', (request,response) => {
  response.json(persons);
@@ -38,6 +41,31 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id);
     response.status(204).end()
 
+})
+
+app.post('/api/persons', (request,response) => {
+  const person = request.body;
+  console.log(person);
+  const {name, number} = person;
+  console.log(name);
+
+  if(!name) {
+      return response.status(404).json({ 
+        error: 'content missing or incorrect content' 
+      });
+  }
+
+  const personAlreadyExists = persons.find(person => person.name.toLowerCase() === name.toLowerCase())
+  console.log(personAlreadyExists)
+  if(personAlreadyExists){
+      return response.status(404).json({
+          error: 'name must be unique'
+      })
+  }
+
+  person.id = generateRandomId()
+  persons = persons.concat(person);
+ response.json(person);
 })
 const PORT = 3001
 app.listen(PORT, () => {
