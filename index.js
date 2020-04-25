bdbd52b490b2f2271847a78b71405b769e586450
Your Name <you@example.com>
2020-04-25 10:34:13 +1000

    Completed step3 and step4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 //     response.send("<h1>Hello</h1>")
 // })
 
-const persons = [ { name: 'Arto Hellas', number: '040-123456', id:1 },
+let persons = [ { name: 'Arto Hellas', number: '040-123456', id:1 },
 { name: 'Ada Lovelace', number: '39-44-5323523', id:2 },
 { name: 'Dan Abramov', number: '12-43-234345', id:3  },
 { name: 'Mary Poppendieck', number: '39-23-6423122' , id:4}];
@@ -23,6 +23,22 @@ app.get('/info', (request, response) => {
     response.send(infoResponse)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+ const id = Number(request.params.id);
+ const person = persons.find(person => person.id === id);
+ if(person) {
+     response.json(person);
+ } else {
+     response.status(404).end();
+ }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id !== id);
+    response.status(204).end()
+
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
