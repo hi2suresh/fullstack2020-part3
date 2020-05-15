5b0955fe0971dde699c4eb0824f7acab6482b45e
Your Name <you@example.com>
2020-05-16 09:51:26 +1000

    Completed excerise for Part3b - Deploying app on internet

--- README.md
+++
@@ -1,2 +0,0 @@
-# fullstack2020-part3
-Excercises starting from Part3 of FullStackCourse from Helsiniki University
---
+++ person-backend/build/precache-manifest.3d02ba7828bb75e675808b219469f727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c4074e24f3870d9e293b764dbadc5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3c484d7db90edcabbde",
+    "url": "/static/css/main.1a7e909f.chunk.css"
+  },
+  {
+    "revision": "cd952c2db9dab78c63c9",
+    "url": "/static/js/2.3bbf1fe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3bbf1fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c484d7db90edcabbde",
+    "url": "/static/js/main.a37d7c91.chunk.js"
+  },
+  {
+    "revision": "376095de1f9e6af1e6bf",
+    "url": "/static/js/runtime-main.18f31ee0.js"
+  }
+]);
