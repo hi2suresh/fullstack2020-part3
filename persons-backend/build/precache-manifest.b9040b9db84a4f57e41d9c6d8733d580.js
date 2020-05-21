5b7e1e004de4b131651a4227078a3a5fe854f7d6
Your Name <you@example.com>
2020-05-21 13:52:31 +1000

    Completed excercises 3.12 to 3.19

---
+++ persons-backend/build/precache-manifest.b9040b9db84a4f57e41d9c6d8733d580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20298aa1f23f43089113f8feb26364d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75d7dc5ce73aa2902b67",
+    "url": "/static/css/main.d50541b0.chunk.css"
+  },
+  {
+    "revision": "72b5fb76322c4f493086",
+    "url": "/static/js/2.3a725ec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a725ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d7dc5ce73aa2902b67",
+    "url": "/static/js/main.23ae8e55.chunk.js"
+  },
+  {
+    "revision": "376095de1f9e6af1e6bf",
+    "url": "/static/js/runtime-main.18f31ee0.js"
+  }
+]);
---
+++ persons-backend/models/person.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+
+  const personSchema = new mongoose.Schema({
+    name:{
+      type: String,
+      minlength: 3,
+      unique: true,
+      required: true
+    },
+    number: {
+      type: Number,
+      min : 10000000,
+      max : 99999999,
+    required: true
+    }
+      
+  })
+
+  personSchema.plugin(uniqueValidator);
+
+  personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+  
+
+  const Person = mongoose.model('Person', personSchema);
+
+  module.exports = mongoose.model('Person', personSchema);
+
+
